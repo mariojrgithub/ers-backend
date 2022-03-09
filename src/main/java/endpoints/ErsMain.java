d10f214d2925c16f71f5f68bff5c5ab2f8358c04
Mario Jr <mariosanchezjr12@gmail.com>
2022-03-08 21:51:27 -0800

    added /api to the endpoints

--- src/main/java/endpoints/ErsMain.java
+++ src/main/java/endpoints/ErsMain.java
@@ -22,38 +22,38 @@ public class ErsMain {
 		System.out.println("Service listening on port " + myServer.port());
 
 		// fetch all employees
-		myServer.get("/manager/all-employees", ctx -> {
+		myServer.get("/api/manager/all-employees", ctx -> {
 			List<EmployeePojo> allEmployees = employeeService.fetchAllEmployees();
 			ctx.json(allEmployees);
 		});
 
 		// fetch all requests
-		myServer.get("/requests", ctx -> {
+		myServer.get("/api/requests", ctx -> {
 			List<RequestPojo> allRequests = requestService.fetchAllRequests();
 			ctx.json(allRequests);
 		});
 
 		// fetch all pending requests
-		myServer.get("/requests/pending", ctx -> {
+		myServer.get("/api/requests/pending", ctx -> {
 			List<RequestPojo> allPendingRequests = requestService.fetchPendingRequests();
 			ctx.json(allPendingRequests);
 		});
 
 		// fetch all resolved requests
-		myServer.get("/requests/resolved", ctx -> {
+		myServer.get("/api/requests/resolved", ctx -> {
 			List<RequestPojo> allResolvedRequests = requestService.fetchResolvedRequests();
 			ctx.json(allResolvedRequests);
 		});
 
 		// fetch requests by ID
-		myServer.get("/requests/{id}", ctx -> {
+		myServer.get("/api/requests/{id}", ctx -> {
 			String id = ctx.pathParam("id");
 			List<RequestPojo> rqById = requestService.fetchEmployeeRequests(Integer.parseInt(id));
 			ctx.json(rqById);
 		});
 
 		// add a request
-		myServer.post("/requests/add", ctx -> {
+		myServer.post("/api/requests/add", ctx -> {
 			RequestPojo newRequest = ctx.bodyAsClass(RequestPojo.class);
 			RequestPojo returnedRequest = employeeService.createNewRequest(newRequest);
 
@@ -62,7 +62,7 @@ public class ErsMain {
 		});
 		
 		// login employee
-		myServer.get("/login", ctx -> {
+		myServer.get("/api/login", ctx -> {
 			EmployeePojo login = ctx.bodyAsClass(EmployeePojo.class);
 			EmployeePojo returnedLogin = employeeService.loginEmployee(login);
 			
